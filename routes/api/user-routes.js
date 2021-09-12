57d359f42fd81b67de2f04be605ac382d92439a3
shannon_p <snp1284kasey@aol.com>
2021-09-12 19:29:00 -0400

    add bcrypt to hash passwords

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -59,7 +59,9 @@ router.put("/:id", (req, res) => {
         // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
         // if req.body has exact key/value pairs to match, you can just use `req.body` instead
+        // pass in req.body instead to only updated what's passed through
         User.update(req.body, {
+            individualHooks: true,
             where: {
                 id: req.params.id
             }
