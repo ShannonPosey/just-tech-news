82a35eb00d578d8900d679b3b98e469a994a3dc1
shannon_p <snp1284kasey@aol.com>
2021-09-21 18:34:20 -0400

    update

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
 const path = require("path");
 const express = require("express");
-const routes = require("./controllers");
-const sequelize = require("./config/connection");
-
-// Handlebars Template
 const exphbs = require("express-handlebars");
-const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const routes = require("./controllers");
+
+const sequelize = require("./config/connection");
+
+const hbs = exphbs.create({});
+
+
+// Handlebars Template Engine
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, "public")));
 
-// Handlebars Template Engine
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
 
 // turn on routes
 app.use(routes);
